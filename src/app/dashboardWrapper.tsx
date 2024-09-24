fca501c1e7d37c905a9fd2f8b1d5373cf25a5bc0
Logan Qiu <chiu.logan@gmail.com>
2024-09-24 15:21:55 -0700

    wrap the DashboardLayout with store provider

--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -1,15 +1,29 @@
 import Navbar from "./(components)";
 import Sidebar from "./(components)/Sidebar";
+import StoreProvider from "./redux";
 
-const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
-  return <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
-    {/* Sidebar */}
-    <Sidebar />
-    <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}>
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
+      {/* Sidebar */}
+      <Sidebar />
+      <main
+        className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}
+      >
         {/* Navbar */}
         <Navbar />
         {children}
-    </main>
-  </div>;
+      </main>
+    </div>
+  );
 };
+
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
+  );
+};
+
 export default DashboardWrapper;
