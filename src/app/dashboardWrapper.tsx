7731bd7a6d6560ee0fb6ef75de585f010f07d27b
Logan Qiu <chiu.logan@gmail.com>
2024-09-24 11:55:54 -0700

    added static imgs and implemented skeleton for sidebar

--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -1,9 +1,10 @@
 import Navbar from "./(components)";
+import Sidebar from "./(components)/Sidebar";
 
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
     {/* Sidebar */}
-    sidebar
+    <Sidebar />
     <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}>
         {/* Navbar */}
         <Navbar />
