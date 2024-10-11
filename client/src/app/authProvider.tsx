2921ebf85cab0f271ec9455d4d43d461cb757397
Logan Qiu <chiu.logan@gmail.com>
2024-10-11 01:04:18 -0700

    added styling on amplify auth login

--- client/src/app/authProvider.tsx
+++ client/src/app/authProvider.tsx
@@ -42,9 +42,16 @@ const formFields = {
   },
 };
 
+const containerStyle: React.CSSProperties = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "100vh",
+};
+
 const AuthProvider = ({ children }: any) => {
   return (
-    <div>
+    <div style={containerStyle}>
       <Authenticator formFields={formFields}>
         {({ user }: any) =>
           user ? (
